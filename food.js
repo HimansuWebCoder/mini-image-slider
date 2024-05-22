cb9d985f22796184d33fe7ffe6002155eff106b6
HimansuWebCoder <naikhimansu546@gmail.com>
2024-05-22 14:36:47 +0530

    image-slider mini project added

---
+++ food.js
@@ -0,0 +1,19 @@
+const fruits = [
+	"images/apple.jpeg",
+	"images/lichi.jpg",
+	"images/mango.jpeg",
+	"images/orange.jpg",
+  ];
+  const veg = [
+	"images/brinjal.jpeg",
+	"images/potato.jpg",
+	"images/carrot.jpeg",
+	"images/tomato.jpeg",
+  ];
+  const nonVeg = [
+	"images/egg.jpeg",
+	"images/mutton.jpeg",
+	"images/chicken.jpg",
+  ];
+
+  export { fruits, veg, nonVeg};
